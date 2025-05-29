aabb5e97bd06fc8a12fc2bc088492d10d1d5a1f3
Fred Bahr <flbahr@mbari.org>
2025-05-29 09:45:07 -0700

    new directory checking scripts

---
+++ scripts/check_dir.py
@@ -0,0 +1,20 @@
+#
+import pandas as pd
+import os
+# check if a subdirectory exist and if not create it?
+def check_dir(basedir,filedate,maxminutes):
+    # reformat filedate to a timestring and also check if the time is within x minutes of maxminutes
+    # round time to nearest maxminutes.
+    ptime=pd.to_datetime(filedate)
+    rtime=ptime.round(f'{maxminutes}min')
+    # check if rounded time is below ptime or above.
+    if rtime < ptime:
+        rtime = rtime - pd.Timedelta(minutes=maxminutes)
+    elif rtime >= ptime:
+        rtime = rtime + pd.Timedelta(minutes=maxminutes)
+    rstime=rtime.strftime('%Y%m%dT%H%M%S')
+    # create the directory path
+    dirpath=os.path.join(basedir,rstime)
+    if not os.path.exists(dirpath):
+        os.makedirs(dirpath)
+    return dirpath
