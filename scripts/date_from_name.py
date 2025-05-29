63d894a1aa4d3f09fe550ad00d1fe3414211dc56
Fred Bahr <flbahr@mbari.org>
2025-05-29 14:10:03 -0700

    updated file

--- scripts/date_from_name.py
+++ scripts/date_from_name.py
@@ -1,15 +1,19 @@
 import datetime as dt
 import pytz
-def date_from_name(filename,mangnification):
+import pdb
+def date_from_name(filename,magnification):
     """
     Extracts the date from a planktivore filename'.
     """
     timezone=pytz.timezone('UTC')
     parts = filename.split('-')
     try:
-        magind=parts.index(mangnification)
-        ltime=parts[magind+1]/1000000
-        imagetime=dt.datetime.fromtimestamp(x).astimezone(timezone)
+        #pdb.set_trace()
+        magind=[idx for idx, part in enumerate(parts) if magnification in part][0]
+
+        #magind=parts.index(magnification)
+        ltime=int(parts[magind+1])/1000000
+        imagetime=dt.datetime.fromtimestamp(ltime).astimezone(timezone)
         return imagetime
     except:
         # file does not contain a time.
