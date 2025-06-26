8e2b548e6f30c6f1153f53308088420fbd6095fd
Fred Bahr <flbahr@mbari.org>
2025-06-26 10:03:56 -0700

    cleaned up code

--- scripts/date_from_name.py
+++ scripts/date_from_name.py
@@ -8,13 +8,9 @@ def date_from_name(filename,magnification):
     timezone=pytz.timezone('UTC')
     parts = filename.split('-')
     try:
-        #pdb.set_trace()
         magind=[idx for idx, part in enumerate(parts) if magnification in part][0]
-
-        #magind=parts.index(magnification)
         ltime=int(parts[magind+1])/1000000
         imagetime=dt.datetime.fromtimestamp(ltime).astimezone(timezone)
         return imagetime
     except:
-        # file does not contain a time.
         return None
